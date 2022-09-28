e2cdfb03aabfa5b1ace94cba957e8f9c2f82d4f4
bastianfbr <badmaxyt@gmail.com>
2022-09-28 16:59:37 +0200

    ✨ Reverse function

--- main.cpp
+++ main.cpp
@@ -6,10 +6,22 @@ int sum(int a, int b) {
     return a+b;
 }
 
+void reverse(int &a, int &b) {
+    int c = a;
+    a = b;
+    b = c;
+
+}
 
 int main()
 {
-    cout<< sum(2, 1);
+    cout<< sum(2, 1) << "\n";
+    int val_1 = 2;
+    int val_2 = 1;
+    int val_3 = 3;
+    reverse(val_1, val_2);
+    cout << "Val 1 : " << val_1 << "\n" << "Val 2 : " << val_2;
+    // Val 1 vaut 1, Val 2 vaut 2 et Val 3 vaut 3
 
     return 0;
 }
