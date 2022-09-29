22d4689df9c68d9158a32f61dabb7d6eb2f4f847
bastianfbr <badmaxyt@gmail.com>
2022-09-29 09:51:26 +0200

    ✨ addAndReverse_r & p functions

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@ void reverse(int &a, int &b) {
 
 }
 
+void addAndReverse_p(int* a, int* b, int* c) {
+    *c = *a + *b;
+}
+
+void addAndReverse_r(int &a, int &b, int &c) {
+    c = a + b;
+}
+
 int main()
 {
     cout<< sum(2, 1) << "\n";
@@ -22,6 +30,11 @@ int main()
     reverse(val_1, val_2);
     cout << "Val 1 : " << val_1 << "\n" << "Val 2 : " << val_2;
     // Val 1 vaut 1, Val 2 vaut 2 et Val 3 vaut 3
-
+    cout << "Val 1 : " << val_1 << "\n" << "Val 2 : " << val_2;
+    // puis on affecte la troisième valeur à la somme des deux premières
+        // ici avec des pointeurs
+    addAndReverse_p(&val_1, &val_2, &val_3);
+        // ici avec des références
+    addAndReverse_r(val_1, val_2, val_3);
     return 0;
 }
