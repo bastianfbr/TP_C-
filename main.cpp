350fe40aeec0e70cdb74d6ff971cb6574ff072d4
bastianfbr <badmaxyt@gmail.com>
2022-10-10 23:11:50 +0200

    ✨ devinerNombre()

--- main.cpp
+++ main.cpp
@@ -19,11 +19,26 @@ string demanderNomPrenom() {
 }
 
 void devinerNombre() {
+    srand(time(NULL)); // initialisation du générateur de random
+    int nb = rand() % 1001;
+    int input = 1010101010;
+    int nbEssais = 0;
+    do {
+        cout << "Devine le nombre :) \n";
+        cout << "Tentative numéro " << nbEssais << "\n";
+        cin >> input;
 
+        if (input > nb) cout << "C'est plus petit ! \n";
+        if (input < nb) cout << "Essaie plus grand ? \n";
+        if (input == nb) cout << "Félicitations, tu as trouvé le nombre !"
+                                 "c'était " << nb << "\n ... avec " << nbEssais << " essais";
+        nbEssais++;
+    } while(input != nb);
 }
 
 int main()
 {
    string nomPrenom = demanderNomPrenom();
-   cout << "Bonjour " << nomPrenom;
+   cout << "Bonjour " << nomPrenom << "\n";
+   devinerNombre();
 }
